4445d54afcd71f792519f745660df90607f975aa
Kenneth Tablang <kennethreytablang@gmail.com>
2026-07-17 20:21:39 +0800

    Migrations update

---
+++ SENGENSystem.Server/Common/Persistence/Migrations/20260717121033_SystemParametersDomainMigrate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SENGENSystem.Server.Common.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SystemParametersDomainMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    SectionCapacityCap = table.Column<int>(type: "int", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedByUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemSettings", x => x.Id);
+                    table.CheckConstraint("CK_SystemSettings_Singleton", "[Id] = 1 AND [SectionCapacityCap] >= 1");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SystemSettings");
+        }
+    }
+}
